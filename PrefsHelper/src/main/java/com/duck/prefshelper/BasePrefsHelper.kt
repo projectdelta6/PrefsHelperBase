0fa96398dad2f4f8fd85d61fda3c99b1a7eacdc4
Bradley Duck <projectdelta6@gmail.com>
2023-09-19 09:09:26 +0100

    Added Generic Enum get and set methods

--- PrefsHelper/src/main/java/com/duck/prefshelper/BasePrefsHelper.kt
+++ PrefsHelper/src/main/java/com/duck/prefshelper/BasePrefsHelper.kt
@@ -1,6 +1,9 @@
 package com.duck.prefshelper
 
 import android.content.SharedPreferences
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.time.LocalDate
@@ -79,6 +82,7 @@ abstract class BasePrefsHelper {
 		return sharedPreferences.getBoolean(key, defValue)
 	}
 
+	@RequiresApi(Build.VERSION_CODES.O)
 	fun setLocalDateTime(key: String, value: LocalDateTime?) {
 		with(sharedPreferences.edit()) {
 			putLong(key, value?.toEpochSecond(ZoneOffset.UTC) ?: -1L)
@@ -86,11 +90,13 @@ abstract class BasePrefsHelper {
 		}
 	}
 
+	@RequiresApi(Build.VERSION_CODES.O)
 	fun getLocalDateTime(key: String): LocalDateTime? {
 		val time = sharedPreferences.getLong(key, -1L)
 		return if(time == -1L) null else LocalDateTime.ofEpochSecond(time, 0, ZoneOffset.UTC)
 	}
 
+	@RequiresApi(Build.VERSION_CODES.O)
 	fun setLocalDate(key: String, value: LocalDate?) {
 		with(sharedPreferences.edit()) {
 			putLong(key, value?.toEpochDay() ?: -1L)
@@ -98,11 +104,13 @@ abstract class BasePrefsHelper {
 		}
 	}
 
+	@RequiresApi(Build.VERSION_CODES.O)
 	fun getLocalDate(key: String): LocalDate? {
 		val time = sharedPreferences.getLong(key, -1L)
 		return if(time == -1L) null else LocalDate.ofEpochDay(time)
 	}
 
+	@RequiresApi(Build.VERSION_CODES.O)
 	fun setLocalTime(key: String, value: LocalTime?) {
 		with(sharedPreferences.edit()) {
 			putLong(key, value?.toSecondOfDay()?.toLong() ?: -1L)
@@ -110,8 +118,23 @@ abstract class BasePrefsHelper {
 		}
 	}
 
+	@RequiresApi(Build.VERSION_CODES.O)
 	fun getLocalTime(key: String): LocalTime? {
 		val time = sharedPreferences.getLong(key, -1L)
 		return if(time == -1L) null else LocalTime.ofSecondOfDay(time)
+	}
+
+	fun setEnum(key: String, value: Enum<*>?) {
+		setString(key, value?.name ?: "")
+	}
+
+	inline fun <reified T : Enum<*>> getEnum(enumClass: Class<T>, key: String): T? {
+		val value = getString(key)
+		return if (value.isBlank()) null else try {
+			enumClass.enumConstants?.firstOrNull { it.name == value }
+		} catch (e: Exception) {
+			Log.w("BasePrefsHelper", "Could not get Enum of ${enumClass.simpleName} for \"$value\"", e)
+			null
+		}
 	}
 }
